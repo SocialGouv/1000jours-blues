af0c4de2618f3056a5730c06e86d652410c9e148
Lebret Audrey <alebret@ippon.fr>
2022-11-17 11:58:12 +0100

    fix(security): config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
+const securityHeaders = [
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  }
+]
+
 module.exports = {
   webpack: function (config) {
     config.module.rules.push({
@@ -5,5 +20,14 @@ module.exports = {
       use: "js-yaml-loader",
     });
     return config;
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
   },
 };
